eba112a75a9e5669d70e98d72be6a177a2f22150
TimothyRJones <timothyrjones32@gmail.com>
2024-07-25 17:57:36 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "paste_your_api_url_here"
+    API_URL: "https://cnt-cc60608b-4e14-400c-bb65-048ce0821b80.containerhub.tripleten-services.com"
 }
